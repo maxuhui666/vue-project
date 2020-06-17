23d6a602d966bd40caf6de69f7e1627a13a15f1a
马旭辉 <maxuhui@hornsun.com>
2020-06-17 17:31:56 +0800

    更新helloWorld组件

--- src/assets/filters.js
+++ src/assets/filters.js
@@ -1,13 +1,27 @@
 import Moment from "moment";
-//  转小写
+
+/**
+ *  转小写
+ * @param value
+ * @returns {string}
+ */
 let lower = value => value.toLowerCase();
 
-//  转大写
+/**
+ * 转大写
+ * @param value
+ * @returns {string}
+ */
 let upper = value => value.toUpperCase();
 
-// 日期格式化
+/**
+ * 日期格式化
+ * @param data
+ * @param format
+ * @returns {string}
+ */
 let dateFormat = (data, format) => {
-    return Moment(data).format(format);
+  return Moment(data).format(format);
 };
 
-export {lower, upper, dateFormat};
+export { lower, upper, dateFormat };
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,54 +1,31 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <table border="1">
-      <tr>
-        <th>设备名称</th>
-        <th>设备编号</th>
-        <th>更新时间</th>
-      </tr>
-      <tr v-for="eq in this.equipmentList" v-bind:key="eq.id">
-        <td>{{ eq.deviceName }}</td>
-        <td>{{ eq.number }}</td>
-        <!--使用全局过滤器-->
-        <td>{{ eq.creditedTime | dateFormat("YYYY-MM-DD") }}</td>
-      </tr>
-    </table>
-
-    <el-button type="primary" @click="getData">刷新</el-button>
+    <el-button type="primary">刷新</el-button>
   </div>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String
-  },
   data() {
     return {
-      pageNumber: 2,
-      equipmentList: []
+      constellations: [
+        { value: 1, label: "白羊座" },
+        { value: 2, label: "金牛座" },
+        { value: 3, label: "双子座" },
+        { value: 4, label: "巨蟹座" },
+        { value: 5, label: "狮子座" },
+        { value: 6, label: "处女座" },
+        { value: 7, label: "天秤座" },
+        { value: 8, label: "天蝎座" },
+        { value: 9, label: "射手座" },
+        { value: 10, label: "魔羯座" },
+        { value: 11, label: "水瓶座" },
+        { value: 12, label: "双鱼座" }
+      ]
     };
   },
-  created() {
-    this.getData();
-  },
-  methods: {
-    getData() {
-      this.pageNumber++;
-      this.$http
-        .post(this.$api.test1.url, {
-          pageNumber: this.pageNumber
-        })
-        .then(response => {
-          this.equipmentList = response.data.list;
-        })
-        .catch(error => {
-          alert(error);
-        });
-    }
-  }
+  methods: {}
 };
 </script>
 
